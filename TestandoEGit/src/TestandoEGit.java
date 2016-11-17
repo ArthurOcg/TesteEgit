032c761087765701c1f19ce1b05e8308149f31a3
aluno <aluno@lti09.lti.sistemas>
2016-11-17 12:01:28 -0300

    versao atual

--- TestandoEGit/src/TestandoEGit.java
+++ TestandoEGit/src/TestandoEGit.java
@@ -2,7 +2,7 @@
 public class TestandoEGit {
 
 	public static void main(String[] args) {
-		
+		System.out.println("TesteEGit");
 		System.out.println("Hello world!");
 		System.out.println("Hello world!");
 		System.out.println("Hello world!");
